2f0f96d980ff6b70273e9ce5f432339547f7bb9d
jeffryporter <jporter7@gmail.com>
2016-11-10 07:59:57 -0500

    add todos to controller

--- src/main/java/com/porter/controllers/RestController.java
+++ src/main/java/com/porter/controllers/RestController.java
@@ -3,6 +3,11 @@ package com.porter.controllers;
 /**
  * Created by jeffryporter on 11/7/16.
  */
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController
 {
+    //TODO CRUD for users
+
+    //TODO CRUD for appointments
 }
